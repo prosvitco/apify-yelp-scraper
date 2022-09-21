75a8134b28a5bc0ecd2f9ad663050913aa497e99
prosvitco <prosvitco@gmail.com>
2022-09-21 17:26:54 +0300

    update match

--- src/extractors.js
+++ src/extractors.js
@@ -45,7 +45,7 @@ const yelpBusinessPartial = ($) => {
     let payload = {};
     const html = $.html();
     const jsonMatch = html.match(/<!--(\{".*bizDetailsPageProps.*?\})-->/);
-    const jsonApollo = html.match(/<script data-apollo-state=".*" type="application\/json">(\{.*?\})<\/script>/);
+    const jsonApollo = html.match(/<!--(\{".*ROOT_QUERY.business.*?\})-->/);
 
     console.log(jsonApollo);
 
